02d5480a6d7259a84431df262064fafebe748f49
dinaarnelia <127836859+dinaarnelia@users.noreply.github.com>
2023-03-14 17:03:32 +0800

    sesi 1

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan HTML 1</title>
+</head>
+<body>
+   
+   <h1 align="center">Belajar Web Programming<?="<sup>80</sup>"?></h1> 
+
+   <script>
+       document.write("<h2>Menggunakan Script JavaScript</h2>");
+   </script>
+
+<?php
+    echo "<h3>Menggunakan Script PHP</h3>";
+?>
+
+</body>
+</html>
